73e6f815a60e873c4877ee0db3f9d2ddc10cffb0
Ali A. Yazdi <platoali@gmail.com>
2024-06-07 10:35:51 +0330

    number cores and comment download-speed

--- configuration.nix
+++ configuration.nix
@@ -174,7 +174,7 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
   nix.settings.experimental-features = [ "nix-command" "flakes"];
-
+  nix.settings.cores = 24;
 #  nix.settings.download-speed = 200;
 }
 
