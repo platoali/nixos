bba51a0213de921414e8cc07c950f42229b34a91
Ali A. Yazdi <platoali@gmail.com>
2024-07-02 17:35:07 +0330

    set pdf  mime type to zathura  and flock lock

--- configuration.nix
+++ configuration.nix
@@ -74,6 +74,7 @@
 
     xdg.mime.enable = true;
     xdg.mime.defaultApplications = {
+      "application/pdf" = "org.pwmt.zathura.desktop";
       "text/html" = "org.qutebrowser.qutebrowser.desktop";
       "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
       "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
