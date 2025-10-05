c013f27708558a8c7d3dc67357eb8e55d4da64e9
Ali A. Yazdi <platoali@gmail.com>
2025-10-05 12:35:17 +0330

    change .ssh/config to recommanded config

--- modules/main-user.nix
+++ modules/main-user.nix
@@ -35,9 +35,22 @@ in {
       ];
    
       programs.ssh = {
-
         enable = true ;
+        enableDefaultConfig= false;
+        matchBlocks."*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
         extraConfig = ''
+Host github.com
     AddKeysToAgent yes
     IdentityFile ~/.ssh/id_ed25519-github 
   '';
